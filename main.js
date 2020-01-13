62cf64dea0fbe68b49688b1080239811963b1cb2
lakshmankotiki <kotiki.lakshman1996@gmail.com>
2020-01-13 23:51:22 +0530

    refactored the code and made a new function

--- main.js
+++ main.js
@@ -2,11 +2,36 @@
  * using javascript DOM
  */
 var body = document.querySelector("body");
+var para = document.querySelector("p");
 
 
-// var myH2 = document.createElement("h2");
-// myH2.textContent = "My Own StopWatch";
-// body.appendChild(myH2);
+//count the number of seconds
+var secondsCount = 0;
+
+//global values
+var stopWatch;
+
+/**converting hours, minutes and even seconds to sec */
+function displayCount() {
+    //for 1 hour 3600secs, with this we'll come to know count of hours
+    var hours = Math.floor(secondsCount / 3600);
+    //for 1 min 60secs, with this we'll come to know count of mins
+    var minutes = Math.floor((secondsCount % 3600) / 60);
+    //
+    var seconds = Math.floor(secondsCount % 60);
+
+    var displayHours = (hours < 10) ? '0' + hours : hours;
+    var displayMinutes = (minutes < 10) ? '0' + minutes : minutes;
+    var displaySeconds = (seconds < 10) ? '0' + seconds : seconds;
+
+    var finalTimeFormat = displayHours + ':' + displayMinutes + ':' + displaySeconds;
+
+    para.textContent = finalTimeFormat;
+
+    secondsCount++;
+};
+
+
 var startButton = document.createElement("button");
 var stopButton = document.createElement("button");
 var resetButton = document.createElement("button");
@@ -17,35 +42,20 @@ body.appendChild(startButton);
 body.appendChild(stopButton);
 body.appendChild(resetButton);
 
+
 document.querySelectorAll("button")[0].addEventListener('click', function() {
-    startFunction();
+    stopWatch = setInterval(displayCount, 1000);
+    startButton.disabled = true;
 });
 
 document.querySelectorAll("button")[1].addEventListener("click", function() {
-    stopFunction();
+    clearInterval(stopWatch);
+    startButton.disabled = false;
 });
 
 document.querySelectorAll("button")[2].addEventListener("click", function() {
-    resetFunction();
+    clearInterval(stopWatch);
+    startButton.disabled = false;
+    secondsCount = 0;
+    displayCount();
 });
-
-var startFunction = function() {
-    var d = new Date();
-    var timeFormat = d.toLocaleTimeString();
-    document.querySelector("p").textContent = timeFormat;
-    var clearInt = setInterval(() => {
-        startFunction();
-    }, 1000);
-};
-// var recInterval = setInterval(() => {
-//     startFunction();
-// }, 1000);
-
-var stopFunction = function() {
-    // clearInterval(clearInt);
-    clearInterval().call(clearInt);
-};
-
-var resetFunction = function() {
-    document.querySelector("p").textContent = "00:00:00";
-};
